11b6b68b7dc3d628e204c97f419b1140dc3cce3a
François Colonna <f.colonnacesari@free.fr>
2018-06-21 17:13:41 +0200

    foc.sh  les_nations.fcl un_etre_humain.fcl un_francais.fcl un_scrutin.fcl un_electeur.fcl un_membre_de_l_executif.fcl un_organe_deliberant.fcl executif.fcl  un_texte_legislatif.fcl  un_decret.fcl  une_loi.fcl les_lois.fcl une_ordonnance.fcl president_republique.fcl

--- constitution/files_fcl.mk
+++ constitution/files_fcl.mk
@@ -1,9 +1,14 @@
 FOCALIZE_SRC=\
+  les_nations.fcl \
   un_etre_humain.fcl \
   un_francais.fcl \
   un_scrutin.fcl \
   un_electeur.fcl \
   un_membre_de_l_executif.fcl \
+  un_organe_deliberant.fcl \
+  executif.fcl \
+  un_texte_legislatif.fcl \
+  un_decret.fcl \
   une_loi.fcl \
   les_lois.fcl \
   une_ordonnance.fcl \
