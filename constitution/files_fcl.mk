13949cd4c8670cd8b1dba0b6c7935d4b93fc1a26
François Colonna <f.colonnacesari@free.fr>
2018-06-23 19:36:39 +0200

    pb avec la_souverainete_nationale.fcl

--- constitution/files_fcl.mk
+++ constitution/files_fcl.mk
@@ -1,17 +1,16 @@
 FOCALIZE_SRC=\
+  basic_sets.fcl \
   un_texte_legislatif.fcl \
   un_etre_humain.fcl \
   un_francais.fcl \
   un_scrutin.fcl \
   un_electeur.fcl \
+  un_acte_de_l_executif.fcl \
   un_membre_de_l_executif.fcl \
   un_membre_du_gouvernement.fcl \
-  un_elu.fcl \
   un_organe_deliberant.fcl \
-  une_loi.fcl \
-  les_lois.fcl \
-  un_acte_de_l_executif.fcl \
-  premier_ministre.fcl \
-  gouvernement.fcl \
-  president_republique.fcl \
+  l_assemblee_nationale.fcl \
+  le_gouvernement.fcl \
+  la_souverainete_nationale.fcl \
+  le_president_republique.fcl \
 
