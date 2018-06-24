e9abad70c8384e1fccc6df782ffd458b379f8aa7
François Colonna <f.colonnacesari@free.fr>
2018-06-24 09:27:04 +0200

    foc.sh basic_sets.fcl un_texte_legislatif.fcl un_etre_humain.fcl un_francais.fcl un_scrutin.fcl un_electeur.fcl un_acte_de_l_executif.fcl  un_membre_de_l_executif.fcl un_membre_du_gouvernement.fcl un_organe_deliberant.fcl une_loi.fcl un_parlementaire.fcl un_depute.fcl l_assemblee_nationale.fcl le_gouvernement.fcl le_parlement.fcl la_souverainete_nationale.fcl le_premier_ministre.fcl le_president_republique.fcl

--- constitution/files_fcl.mk
+++ constitution/files_fcl.mk
@@ -9,8 +9,13 @@ FOCALIZE_SRC=\
   un_membre_de_l_executif.fcl \
   un_membre_du_gouvernement.fcl \
   un_organe_deliberant.fcl \
+  une_loi.fcl \
+  un_parlementaire.fcl \
+  un_depute.fcl \
   l_assemblee_nationale.fcl \
   le_gouvernement.fcl \
+  le_parlement.fcl \
   la_souverainete_nationale.fcl \
+  le_premier_ministre.fcl \
   le_president_republique.fcl \
 
