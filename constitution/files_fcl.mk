771620cb68ad946dd76186b39f48867b83187242
François Colonna <f.colonnacesari@free.fr>
2018-06-24 16:45:53 +0200

    tout compile avec heritage. Conversions à faire ?

--- constitution/files_fcl.mk
+++ constitution/files_fcl.mk
@@ -1,21 +1,35 @@
 FOCALIZE_SRC=\
   basic_sets.fcl \
+  un_territoire.fcl \
   un_texte_legislatif.fcl \
-  un_etre_humain.fcl \
-  un_francais.fcl \
-  un_scrutin.fcl \
-  un_electeur.fcl \
-  un_acte_de_l_executif.fcl \
-  un_membre_de_l_executif.fcl \
-  un_membre_du_gouvernement.fcl \
   un_organe_deliberant.fcl \
   une_loi.fcl \
+  une_ordonnance.fcl \
+  un_decret.fcl \
+  un_etre_humain.fcl \
+  un_francais.fcl \
+  le_peuple_francais.fcl \
+  un_scrutin.fcl \
+  un_scrutin_direct.fcl \
+  un_scrutin_indirect.fcl \
+  un_electeur.fcl \
+  l_electorat.fcl \
+  une_circonscription.fcl \
+  un_elu.fcl \
   un_parlementaire.fcl \
+  le_parlement.fcl \
   un_depute.fcl \
+  un_senateur.fcl \
+  le_senat.fcl \
+  un_acte_de_l_executif.fcl \
+  un_membre_de_l_executif.fcl \
+  l_executif.fcl \
+  un_ministre.fcl \
+  un_membre_du_gouvernement.fcl \
+  le_premier_ministre.fcl \
   l_assemblee_nationale.fcl \
   le_gouvernement.fcl \
-  le_parlement.fcl \
+  la_constitution.fcl \
   la_souverainete_nationale.fcl \
-  le_premier_ministre.fcl \
   le_president_republique.fcl \
-
+ 
