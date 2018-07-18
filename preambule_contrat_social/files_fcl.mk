fb6066f10fe1a59cb68150e15a01370460ce84dc
François Colonna <f.colonnacesari@free.fr>
2018-07-18 15:42:50 +0200

    retour au Parlement

--- preambule_contrat_social/files_fcl.mk
+++ preambule_contrat_social/files_fcl.mk
@@ -11,35 +11,10 @@ FOCALIZE_SRC=\
   Un_Etre_humain_S.fcl \
   Une_Zone_geographique_S.fcl \
   Un_Creneau_horaire_S.fcl \
-  Un_Axiome_de_l_interet_general_S.fcl \
   Une_Institution_politique_S.fcl \
   Un_Territoire_S.fcl \
   Un_Theme_politique_S.fcl \
   Une_Doleance_S.fcl \
   Une_Loi_S.fcl \
   Une_Proposition_de_loi_S.fcl \
-  Une_Note_S.fcl \
-  Une_Assertion_constitutionnelle_S.fcl \
-  Les_Fonctions_de_conversion.fcl \
-  Un_Bloc_S.fcl \
-  Un_Bloc_genesis_S.fcl \
-  Un_Bloc_terminal_S.fcl \
-  Un_Bloc_assertion_S.fcl \
-  Une_Blockchain_S.fcl \
-  Une_Blockchain_arbre_S.fcl \
-  Une_Blockchain_doleance_S.fcl \
-  Une_Blockchain_definition_S.fcl \
-  Une_Blockchain_propriete_S.fcl \
-  Une_Blockchain_constitution_S.fcl \
-  Une_Doleance_S.fcl \
-  Un_Noeud_S.fcl \
-  Un_Noeud_citoyen_S.fcl \
-  Un_Reseau_S.fcl \
-  Un_Sous_reseau_S.fcl \
-  Le_Reseau_constituant_S.fcl \
-  Un_Territoire_francais_S.fcl \
-  Un_Citoyen_S.fcl \
-  La_Souverainete_d_un_citoyen_S.fcl \
-  La_Souverainete_des_citoyens_S.fcl \
-  
  
