9ea094ef414cd1f4a567f836f73bca04e2e9776a
Gustave Nimant <f.colonnacesari@free.fr>
2018-08-29 17:25:18 +0200

    ajout de Les_Representants_du_peuple_S. Inutilisé. Avant révision architecture selon la nécessité

--- utilities/foc.sh
+++ utilities/foc.sh
@@ -13,12 +13,16 @@ fi
 echo "FOCALIZE_SRC=\\" > files_fcl.mk
 for i in $args
 do
-    ok=`echo $i | grep -c '.fcl'`
-    if [ "$ok" == "0" ]
+    if [ `echo $i | grep -c '.fcl'` != "0" ]
     then
-	echo "  $i.fcl \\" >> files_fcl.mk
-    else
 	echo "  $i \\" >> files_fcl.mk
+    else
+	if [ `echo $i | grep -c '\.'` == "0" ]
+	then
+	    echo "  ${i}.fcl \\" >> files_fcl.mk
+	else
+	    echo "  ${i}fcl \\" >> files_fcl.mk
+	fi
     fi
 done
 echo "" >> files_fcl.mk
