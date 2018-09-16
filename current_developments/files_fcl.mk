64af1caced22b5046974c834b5ba243b0cd9cd91
Gustave Nimant <f.colonnacesari@free.fr>
2018-09-16 18:47:10 +0200

    création de Article_27_S

--- current_developments/files_fcl.mk
+++ current_developments/files_fcl.mk
@@ -1,3 +1,7 @@
 FOCALIZE_SRC=\
-  Un_Fait_de_pauvrete_S.fcl \
+  Un_Citoyen_electeur_subtype_S.fcl \
+  Le_Parlement_S.fcl \
+  Le_Gouvernement_S.fcl \
+  L_Assemblee_nationale_S.fcl \
+  Article_27_S.fcl \
 
